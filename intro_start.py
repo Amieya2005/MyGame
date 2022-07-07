07a8a41a5abd6396cad1bcd69132b441ead029b9
Amieya <cudjoeamieya7@gmail.com>
2022-07-06 22:31:26 -0400

    this code works. :)

---
+++ intro_start.py
@@ -0,0 +1,54 @@
+def newFunction(option):
+    # notice I am passing something and reading whats returned. This can be
+    # useful for putting different chapters of your story in modules and having
+    # them keep track of how your story is progressing
+
+    # Expand with all types of options here, be creative! pay attention to how
+    # different the code looks vs what you see in the terminal. The person you
+    # share your game with will not see your code when interacting with it.
+
+    # you can ask things like math questions and make choices on if the user gets
+    # it right or not.
+    #userInput= input("The user input:")
+    #if (userInput == "Y"):
+        #print(userInput)
+
+#localUserInput = input("please enter something new")
+
+# add this so it can began the back story
+    #print("___" , option)
+    localuserInput = input("Are you sure? y/n? ")
+    if localuserInput == "y":
+            print("""You are a theif and his a skill for pickpoketing.
+        This was all until you decided to pickpoket one of the royal princes.
+        The prince and the guards caught you and you are now being dragged away to their dungon ...""")
+
+            print(""" This dungon is being at the top of the castle. You weren't chained to the wall like you heard in the rumors or in other countries (kinda dumb if you ask me. I'm just saying.).
+        They just threw you in a enclosed room surrounded with nothing but old bricks that had been dying for years and a thick rusty metal bar that has a small vertical opening for the guards to slide mush food
+        that had been out for weeks.
+        (personally i wouldn't touch that only eat food from my mom, Donato and his girlfriend <3 and Gordan Ramsay but that's just me).
+        All until you saw something in the food that you where able to use to pick the lock (You welcome). You took your oportunity and escaped while they weren't looking...
+
+                Now you are a lone theif in the night sneaking around the castle looking for an escape...
+
+
+            Dont Get Caught.
+            Get as many gems and Jewls as you can.
+            But what ever you do don't die.
+            .......
+
+            Good luck, See you on the other side!
+
+
+            """ )
+
+    elif localuserInput == "n":
+        print("ok...")
+
+
+    return localuserInput
+
+#import random
+# this is so you can roll the dice 1-20 sided and get the chocies
+
+    #print(")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # This is a starter for a text based game in python.
 # fill it in and tell a story that would be fun to play
 
-from expand import *
+from intro_start import *
+from low_rank_monsters import *
 # I use multiple modules as an example for you to try and replicate to keep your
 # story neat and get used to using them
 
@@ -17,33 +18,44 @@ def main():
    while(endStory > 1):
        # terminal prints can be over multiple lines using """ print """ format,
        # instead of the regular  "print" give it a try
-       print("this is the start of a story, You could pass things to main to include multiple items")
+       print("this is the start of a story, your story. This time you are the one taking control...")
        # this is how your story can take input and read it back out
-       userInput = input("Please enter something: ")
-       print("user input: ", userInput)
+       userInput = input("What is your name?: ")
+       print("Nice to meet you" + userInput + """My name is ___. This may be the first and last time may ever hear from me...
+       Or I may be of guidence
+       or my not.
+       The choice is yours...""")
        # notice input can output to terminal. Look up the input function for python
        # to see why.
-       userInput = input("""neat, now enter something from the choices i give you
+       userInput = input("""Now choose from the choices below:
                  option 1: enter a F to end
-                 option 2: enter a C to continue """)
+                 option 2: enter a C to continue
+                  """)
        # there are many ways to break this and you should try to write code that
        # catches common cases and check the user for options other than what
        # asked for.
-       if(userInput == "C"):
-           print("into the expanded module this code will go")
+       #EndStory > 1
+
+        # print("Are you Sure?")
+         #userInput = input(""" y/n?   """)
+     # if (userInput == "y"):
+       if userInput :="C" or userInput == "c":
+           print("You are a brave one. Well please follow me then ...")
            moduleReturns = newFunction(userInput)
-           print("wow you really put ", moduleReturns)
-           print("goodbye, rerun python main.py to play")
+           #moduleReturns = NewFunction(mMonsters)
+          # print(moduleReturns)
+           print("This is just a demo, thanks you for playing. Goodbye  :)")
+           #print("goodbye, rerun python main.py to play")
 # This ends the story
            endStory = 0
-       elif(userInput == "F"):
-           print("goodbye, return python main.py to play")
+       elif (userInput := "F" or userInput == "f"):
+           print("Goodbye")
+         #if (userInput == "n"):
+             #print("That is okay, Goodbye...")
 # This also ends the story
-           endStory = 0
+           #endStory = 0
 
-       print("the option was case sensitive or you didn't selected from per-determined options")
-
-
+      #print("the option was case sensitive or you didn't selected from per-determined options")
 # Remove this and try to run your code to try and understand what it does so
 # if you write a main module and its not working you can tell why.
 if __name__ == '__main__':
