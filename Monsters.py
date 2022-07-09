8284dccef330fcc1b3b8e2eb3b5be84ff4123b48
Amieya <cudjoeamieya7@gmail.com>
2022-07-09 11:45:51 -0400

    more commits

---
+++ Monsters.py
@@ -0,0 +1,15 @@
+#class Monsters:
+import random
+import math
+
+#def newFuction():
+    #class Troll(Monster):
+def Monster(troll):
+
+# you can roll the dice between 1-5 and this would choos the mosnter the the assinged list
+
+
+
+    if dice == 1:
+        print(" ")
+    return Monsters
